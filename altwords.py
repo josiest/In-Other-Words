30f4ec79c94a06902c89757cb72cd9950e7d80f1
Josie Thompson <josiahst@gmail.com>
2019-11-13 22:59:48 -0800

    finished project enough to submit

--- altwords.py
+++ altwords.py
@@ -6,8 +6,21 @@ import os
 from bs4 import BeautifulSoup
 
 url = 'https://www.foxnews.com/politics'
-fn = 'titles.txt'
+fn = 'titles.md'
 path = os.path.join(os.getcwd(), fn)
+
+def main():
+    response = requests.get(url)
+    if response.status_code != requests.codes.ok:
+        with open(path, 'w') as f:
+            f.write('# Request to Fox News headlines was denied!')
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    titles = list(get_titles(soup))
+
+    while True:
+        run_system(titles)
 
 def get_titles(soup):
     titles = soup.find_all(re.compile('h[1-9]+'), class_='title')
@@ -17,15 +30,26 @@ def get_titles(soup):
             continue
         yield title_soup.contents[0]
 
-def main():
-    response = requests.get(url)
-    if response.status_code != requests.codes.ok:
-        print('request denied')
-        return
+def run_system(titles):
+    for i, title in enumerate(titles):
+        word, replacement = get_response(title)
+        titles[i] = title.replace(word, replacement)
+        with open(path, 'w') as f:
+            f.write('\n'.join('# {}'.format(line) for line in titles))
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    with open(path, 'w') as f:
-        f.write('\n'.join(list(get_titles(soup))))
+def get_response(title):
+    prompt = 'What would you like to replace in:\n\n{}\n\n'.format(title)
+    word = input(prompt)
+
+    while not word or word not in title:
+        word = input("Sorry, I couldn't find {} in the title:\n\n"+
+                     "{}\n\n".format(title)+"What is something else you want "+
+                     "to replace?\n\n")
+
+    replacement = input("What would you like to replace "+
+                        "{} with?\n\n".format(word))
+
+    return word, replacement
 
 if __name__ == '__main__':
     main()
